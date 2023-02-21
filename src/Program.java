8c4f9b22f651e7ff7d5b9e6eedda0ed82279045e
doganes1507 <d.oganes1507@gmail.com>
2023-02-21 20:52:50 +0300

    Added multiply function

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,11 @@ public class Program {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println(multiply(15, 0.5));
+    }
+
+    public static double multiply(double a, double b)
+    {
+        return a * b;
     }
 }
