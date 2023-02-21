0b8a9ce8a1e22794c308aa5c950eaf7428b04c99
doganes1507 <d.oganes1507@gmail.com>
2023-02-21 21:21:54 +0300

    Added function find_divisors, which takes double n number and returns ArrayList<Integer> with all divisors of that number

--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,30 @@
+import java.util.ArrayList;
+
 public class Program {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
         System.out.println(multiply(15, 0.5));
+        System.out.println(find_divisors(24));
     }
 
     public static double multiply(double a, double b)
     {
         return a * b;
+    }
+
+    public static ArrayList<Integer> find_divisors(double n)
+    {
+        var divisors = new ArrayList<Integer>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                divisors.add(i);
+            }
+        }
+
+        return divisors;
     }
 }
