db8c89804fd3fccc6347c754160f3e73bba64e9b
doganes1507 <d.oganes1507@gmail.com>
2023-03-21 22:09:34 +0300

    Created distanceTo and static computeArea methods

--- src/LaboratoryWork2/Point3D.java
+++ src/LaboratoryWork2/Point3D.java
@@ -1,5 +1,8 @@
 package LaboratoryWork2;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Point3D extends Point2D
 {
     protected double coordinateZ;
@@ -17,6 +20,30 @@ public class Point3D extends Point2D
 
     public void setCoordinateZ(double value) { this.coordinateZ = value; }
     public double getCoordinateZ() { return this.coordinateZ; }
+
+    public double distanceTo(Point3D point)
+    {
+        return sqrt(
+                pow(this.coordinateX - point.coordinateX, 2) +
+                pow(this.coordinateY - point.coordinateY, 2) +
+                pow(this.coordinateZ - point.coordinateZ, 2)
+        );
+    }
+
+    public static double computeArea(Point3D point1, Point3D point2, Point3D point3)
+    {
+        if (point1.equals(point2) || point2.equals(point3) || point3.equals(point1))
+        {
+            throw new RuntimeException("Unable to calculate area. Coordinates of two or more points are the same");
+        }
+
+        var a = point1.distanceTo(point2);
+        var b = point2.distanceTo(point3);
+        var c = point3.distanceTo(point1);
+        var halfPerimeter = (a + b + c) / 2;
+
+        return sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+    }
 
     @Override
     public boolean equals(Object obj)
